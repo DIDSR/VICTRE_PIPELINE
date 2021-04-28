999efc04dc678457d73dae0d9dc5d3edb08d18fc
Lago <11331203+malago86@users.noreply.github.com>
2021-04-28 15:10:54 -0400

    Update documentation

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 import sys
 import os
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..Victre/'))
 
 # -- Project information -----------------------------------------------------
 
